5a89d65ab7912f9c7f1a0887c35b207523d75568
Ninki <104530777+Ninkee@users.noreply.github.com>
2023-09-29 18:03:11 +0100

    first commit

---
+++ RockPaperScissors.py
@@ -0,0 +1,16 @@
+import keyboard 
+
+def start():
+    player_input = input("Play?")
+    single()
+
+def single():
+    print("")
+
+
+"""
+while True:
+    keyboard.wait('space')
+    print('space was pressed! Waiting on it again...')     
+""" 
+    
