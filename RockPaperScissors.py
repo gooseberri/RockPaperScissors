d6c9b08ee7b3160abae0677316c381e1c1abad02
Ninki <104530777+Ninkee@users.noreply.github.com>
2023-09-29 18:27:43 +0100

    Created player input function get_player_input()

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,16 +1,17 @@
 import keyboard 
 
 def start():
-    player_input = input("Play?")
-    single()
+    start_input = input("Play?") 
+    player_input = get_player_input()
+    secondary_input = get_secondary_input()
 
-def single():
-    print("")
-
-
-"""
-while True:
-    keyboard.wait('space')
-    print('space was pressed! Waiting on it again...')     
-""" 
-print("SAFAS")
+def get_player_input():
+    valid_input = False
+    while not valid_input:
+        player_move = input("1: Rock\n" "2: Paper\n" "3: Scissors\n")       
+        player_move = int(player_move)
+        if player_move == 1 or player_move == 2 or player_move == 3:
+            valid_input = True
+        else: valid_input = False
+    
+start()
