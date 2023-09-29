68abe9ee91703055d2ac7d2eb4cf10afdb9790b2
Ninki <104530777+Ninkee@users.noreply.github.com>
2023-09-29 19:18:15 +0100

    Added replay

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -3,10 +3,18 @@ from random import randint
 
 
 def start():
-    start_input = input("Play? Press ENTER") 
-    player_input = get_player_move()
-    secondary_input = get_secondary_move()
-    check_winner(player_input, secondary_input)
+    playing = True
+    first_play = True
+    while playing:
+        if first_play:
+            start_input = input("Play? Press ENTER") 
+        player_input = get_player_move()
+        secondary_input = get_secondary_move()
+        check_winner(player_input, secondary_input)
+        continue_input = input("Continue? Y/N")
+        if continue_input.upper() != "Y":
+            playing = False
+        first_play = False  
 
 def get_player_move():
 
@@ -18,6 +26,7 @@ def get_player_move():
             valid_move = True
         else: valid_move = False
     return player_move
+
 def get_secondary_move():
     value = randint(1,3)
     return value
